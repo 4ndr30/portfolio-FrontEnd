e4d424ee2499b3288995a4e602fd7640bf0c95e5
4ndr30 <gabrielaortega89@gmail.com>
2022-11-08 09:05:21 -0300

    add json and js

---
+++ main.js
@@ -0,0 +1,9 @@
+/*const deportista = {
+    nombre: 'Messi',
+    champions: 4,
+}
+
+console.log(deportista);
+
+fetch('http://127.0.0.1:5500/pasajero.json').then((response) => response.json()).then((data)=> console.log(data));
+*/
