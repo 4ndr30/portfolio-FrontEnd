075a37efd9c617a405306a06360f7e8428895441
4ndr30 <gabrielaortega89@gmail.com>
2022-11-19 14:18:37 -0300

    se agregan modulos about me y experiencia

--- portfolioFront/src/app/app.module.ts
+++ portfolioFront/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoARGComponent } from './components/logo-arg/logo-arg.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoARGComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
