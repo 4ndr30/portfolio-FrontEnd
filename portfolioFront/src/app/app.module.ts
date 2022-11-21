5b7103caabb7480abefd760d78cd7cbbe165cf86
4ndr30 <gabrielaortega89@gmail.com>
2022-11-21 07:53:56 -0300

    se agregan componentes en angular

--- portfolioFront/src/app/app.module.ts
+++ portfolioFront/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoARGComponent } from './components/logo-arg/logo-arg.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutMEComponent } from './components/about-me/about-me.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoARGComponent,
     RedesComponent,
-    BannerComponent
+    BannerComponent,
+    AboutMEComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
